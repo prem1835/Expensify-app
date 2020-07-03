86e481777b0264ef60bd01f69e311bc474f693b9
Prem Mali <premmali1835@gmail.com>
2020-07-03 22:43:46 +0530

    Linking between Routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -10,16 +10,36 @@ const HelpPage = () => <div>Help me</div>;
 
 const Homepage = () => <div>Welcome!</div>;
 
-const NotFoundPage = () => <div>404!</div>;
+const NotFoundPage = () => (
+  <div>
+    404! - <Link to="/">Go Home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <Link to="/">Go to Homepage</Link>
+    <br />
+    <Link to="/help">Go to HelpPage</Link>
+    <br />
+    <Link to="/edit">Go to EditPage</Link>
+    <br />
+    <Link to="/error">Go to 404!</Link>
+  </header>
+);
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={Homepage} exact={true} />
-      <Route path="/edit" component={EditExpense} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={NotFoundPage}></Route>
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={Homepage} exact={true} />
+        <Route path="/edit" component={EditExpense} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage}></Route>
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
