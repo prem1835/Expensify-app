0e796f4c7eedbde52db2839d59efac2170fe4500
Prem Mali <premmali1835@gmail.com>
2020-09-10 17:36:50 +0530

    spreading object

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,115 @@
 import { createStore, combineReducers } from "redux";
+import { v4 as uuidv4 } from "uuid";
+
+// ADD_EXPENSES
+const addExpense = ({
+  description = "",
+  note = "",
+  amount = "",
+  createdAt = 0,
+} = {}) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuidv4(),
+    description,
+    note,
+    amount,
+    createdAt,
+  },
+});
+
+// REMOVE_EXPENSES
+const removeExpense = ({ id } = {}) => ({
+  type: "REMOVE_EXPENSE",
+  id,
+});
+
+// EDIT_EXPENSES
+
+const editExpense = (id, updates) => ({
+  type: "EDIT_EXPENSE",
+  updates,
+  id,
+});
+
+// SET_TEXT_FILTER
+const setTextFilter = (filterText = "") => ({
+  type: "SET_TEXT_FILTER",
+  updates: {
+    text: filterText,
+  },
+});
+// Expenses Reducers
+
+const expensesReducersDefaultState = [];
+
+const expensesReducers = (state = expensesReducersDefaultState, action) => {
+  switch (action.type) {
+    case "ADD_EXPENSE":
+      return [...state, action.expense];
+    case "REMOVE_EXPENSE":
+      return state.filter(({ id }) => id !== action.id);
+    case "EDIT_EXPENSE":
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates,
+          };
+        } else {
+          return expense;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+// Filters Reducers
+
+const filtersReducersDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined,
+};
+
+const filtersReducers = (state = filtersReducersDefaultState, action) => {
+  switch (action.type) {
+    case "SET_TEXT_FILTER":
+      return {
+        ...state,
+        ...action.updates,
+      };
+    default:
+      return state;
+  }
+};
+
+// Store Creation
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducers,
+    filters: filtersReducers,
+  })
+);
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(
+  addExpense({ description: "Rent", amount: 100 })
+);
+const expenseTwo = store.dispatch(
+  addExpense({ description: "Coffee", amount: 300 })
+);
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter("rent"));
+store.dispatch(setTextFilter());
 
 const demoState = {
   expenses: [
