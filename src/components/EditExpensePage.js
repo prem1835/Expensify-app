8c5eb404d902f127ee4ad8895069c00890dc2b3d
Prem Mali <premmali1835@gmail.com>
2020-09-21 01:47:49 +0530

    react-redux almost completed

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { editExpense } from "../actions/expenses";
+import ExpenseForm from "./ExpenseForm";
 
 const EditExpensePage = (props) => {
-  console.log(props);
   return (
     <div>
-      Editing the expense with id of {props.match.params.id}
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push("/");
+        }}
+      />
     </div>
   );
 };
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(
+      (expense) => expense.id === props.match.params.id
+    ),
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { removeExpense } from "../actions/expenses";
+
+const ExpenseListItem = ({ description, amount, createdAt, id, dispatch }) => {
+  return (
+    <div>
+      <Link to={`edit/${id}`}>
+        <h3>{description}</h3>
+      </Link>
+      <p>
+        {amount} - {createdAt}
+      </p>
+      <button onClick={() => dispatch(removeExpense({ id }))}>Remove</button>
+    </div>
+  );
+};
+
+export default connect()(ExpenseListItem);
